6d100667f94a8aa4501f16408b863b67776bbced
Ethan McDowell <ethan.r.mcdowell@gmail.com>
2021-02-16 15:44:01 -0500

    Working on ScrollReveal bug

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -5,17 +5,17 @@ import ScrollReveal from "scrollreveal";
 
 function Card(props){
 
-    React.componentDidMount(() => {
-        ScrollReveal().reveal(".portfolio-one", {delay: 300});
-        ScrollReveal().reveal(".portfolio-two", {delay: 350});
-        ScrollReveal().reveal(".portfolio-three", {delay: 450});
-        ScrollReveal().reveal(".portfolio-four", {delay: 500});
-        ScrollReveal().reveal(".portfolio-five", {delay: 550});
-        ScrollReveal().reveal(".portfolio-six", {delay: 600});
-    }, []);
+    // React.useEffect(() => {
+    //     ScrollReveal().reveal(".portfolio-one", {delay: 300});
+    //     ScrollReveal().reveal(".portfolio-two", {delay: 350});
+    //     ScrollReveal().reveal(".portfolio-three", {delay: 450});
+    //     ScrollReveal().reveal(".portfolio-four", {delay: 500});
+    //     ScrollReveal().reveal(".portfolio-five", {delay: 550});
+    //     ScrollReveal().reveal(".portfolio-six", {delay: 600});
+    // }, []);
 
     return(
-        <div className={`load-hidden col-xs-12 col-sm-6 col-lg-4 ${props.class}`}>
+        <div className={`col-xs-12 col-sm-6 col-lg-4 ${props.class}`}>
             <a href={props.href} target="_blank" rel="noreferrer">
                 <div className="card">
                     <img src={process.env.PUBLIC_URL + props.img} alt={props.alt}/>
