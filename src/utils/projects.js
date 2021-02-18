75e2caa6f001cd78d87fc090e6886df952c61b16
ethan <ethan.r.mcdowell@gmail.com>
2021-02-18 13:39:04 -0500

    Corrected error in JSON file
    Was previously linking to incorrect repository for Minion Mingle project.

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -9,7 +9,7 @@ const projects = [
     },{
         id: 2,
         title: "Minion Mingle",
-        href: "https://github.com/ethanrmcdowell/README-Generator",
+        href: "https://github.com/ethanrmcdowell/Dating-Website"
         img: "/images/dating.jpg",
         alt: "dating website demo image",
         class: "portfolio-two"
