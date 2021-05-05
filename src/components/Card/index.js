19cc55eb0d344eac3375577e64ebebebe1ec1e96
Ethan McDowell <ethan.r.mcdowell@gmail.com>
2021-05-05 17:02:17 -0400

    Removed scroll reveal for portfolio section, updated projects with covid dash and vehicle dash projects

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,31 +1,19 @@
-import React from "react";
-import "./style.css";
-import ScrollReveal from "scrollreveal";
+import React from 'react';
+import './style.css';
 
-
-function Card(props){
-
-    React.useEffect(() => {
-        ScrollReveal().reveal(".portfolio-one", {delay: 300});
-        ScrollReveal().reveal(".portfolio-two", {delay: 350});
-        ScrollReveal().reveal(".portfolio-three", {delay: 450});
-        ScrollReveal().reveal(".portfolio-four", {delay: 500});
-        ScrollReveal().reveal(".portfolio-five", {delay: 550});
-        ScrollReveal().reveal(".portfolio-six", {delay: 600});
-    }, []);
-
-    return(
-        <div className={`card-div col-xs-12 col-sm-6 col-lg-4 ${props.class}`}>
-            <a href={props.href} target="_blank" rel="noreferrer">
-                <div className="card">
-                    <img src={process.env.PUBLIC_URL + props.img} alt={props.alt}/>
-                    <div className="card-body">
-                        <h5 className="card-title">{props.title}</h5>
-                    </div>
-                </div>
-            </a>
+function Card(props) {
+  return (
+    <div className={`card-div col-xs-12 col-sm-6 col-lg-4 ${props.class}`}>
+      <a href={props.href} target='_blank' rel='noreferrer'>
+        <div className='card'>
+          <img src={process.env.PUBLIC_URL + props.img} alt={props.alt} />
+          <div className='card-body'>
+            <h5 className='card-title'>{props.title}</h5>
+          </div>
         </div>
-    )
+      </a>
+    </div>
+  );
 }
 
 export default Card;
--- src/utils/projects.js
+++ src/utils/projects.js
@@ -1,47 +1,52 @@
 const projects = [
-    {
-        id: 1,
-        title: "Choose Your Own Adventure",
-        href: "https://github.com/ethanrmcdowell/choose-adventure",
-        img: "/images/chooseadventure.jpg",
-        alt: "choose adventure demo image",
-        class: "portfolio-one"
-    },{
-        id: 2,
-        title: "Minion Mingle",
-        href: "https://github.com/ethanrmcdowell/Dating-Website",
-        img: "/images/dating.jpg",
-        alt: "dating website demo image",
-        class: "portfolio-two"
-    },{
-        id: 3,
-        title: "Employee Tracker",
-        href: "https://github.com/ethanrmcdowell/Employee-Tracker",
-        img: "/images/employee.jpg",
-        alt: "employee tracker demo image",
-        class: "portfolio-three"
-    },{
-        id: 4,
-        title: "Fitness Tracker",
-        href: "https://github.com/ethanrmcdowell/Workout-Tracker",
-        img: "/images/fitness.jpg",
-        alt: "workout tracker demo image",
-        class: "portfolio-four"
-    },{
-        id: 5,
-        title: "Budget Tracker",
-        href: "https://github.com/ethanrmcdowell/Budget-Tracker",
-        img: "/images/budget.jpg",
-        alt: "budget tracker demo image",
-        class: "portfolio-five"
-    },{
-        id: 6,
-        title: "AskKaren",
-        href: "https://github.com/ethanrmcdowell/AskKaren",
-        img: "/images/askkaren.jpg",
-        alt: "ask karen demo image",
-        class: "portfolio-six"
-    }
-]
+  {
+    id: 1,
+    title: 'COVID-19 Dashboard',
+    href: 'https://ethanrmcdowell.github.io/covid-watcher/',
+    img: '/images/coviddash.jpg',
+    alt: 'covid dashboard demo image',
+    class: 'portfolio-one',
+  },
+  {
+    id: 2,
+    title: 'Choose Your Own Adventure',
+    href: 'https://github.com/ethanrmcdowell/choose-adventure',
+    img: '/images/chooseadventure.jpg',
+    alt: 'choose adventure demo image',
+    class: 'portfolio-two',
+  },
+  {
+    id: 3,
+    title: 'Vehicle Dashboard',
+    href: 'https://ethanrmcdowell.github.io/vehicle-dashboard/',
+    img: '/images/vehicle-dash.jpg',
+    alt: 'vehicle dashboard demo image',
+    class: 'portfolio-three',
+  },
+  {
+    id: 4,
+    title: 'Employee Tracker',
+    href: 'https://github.com/ethanrmcdowell/Employee-Tracker',
+    img: '/images/employee.jpg',
+    alt: 'employee tracker demo image',
+    class: 'portfolio-four',
+  },
+  {
+    id: 5,
+    title: 'Fitness Tracker',
+    href: 'https://github.com/ethanrmcdowell/Workout-Tracker',
+    img: '/images/fitness.jpg',
+    alt: 'workout tracker demo image',
+    class: 'portfolio-five',
+  },
+  {
+    id: 6,
+    title: 'Budget Tracker',
+    href: 'https://github.com/ethanrmcdowell/Budget-Tracker',
+    img: '/images/budget.jpg',
+    alt: 'budget tracker demo image',
+    class: 'portfolio-six',
+  },
+];
 
 export default projects;
