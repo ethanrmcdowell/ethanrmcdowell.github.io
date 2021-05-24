69e50e8338ec7af7b41730de96fb43dc28070179
Ethan McDowell <ethan.r.mcdowell@gmail.com>
2021-05-24 14:39:00 -0400

    Updating portfolio projects, created modal to appear when you click on a project

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,19 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
 import './style.css';
 
-function Card(props) {
+const MyModal = props => {
   return (
-    <div className={`card-div col-xs-12 col-sm-6 col-lg-4 ${props.class}`}>
-      <a href={props.href} target='_blank' rel='noreferrer'>
-        <div className='card'>
-          <img src={process.env.PUBLIC_URL + props.img} alt={props.alt} />
-          <div className='card-body'>
-            <h5 className='card-title'>{props.title}</h5>
-          </div>
-        </div>
-      </a>
-    </div>
+    <Modal
+      {...props}
+      size='lg'
+      aria-labelledby='contained-modal-title-vcenter'
+      centered>
+      <Modal.Header closeButton>
+        <Modal.Title id='contained-modal-title-vcenter'>
+          Modal heading
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <h4>{props.title}</h4>
+        <p>
+          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+          consectetur ac, vestibulum at eros.
+        </p>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
   );
-}
+};
+
+const Card = props => {
+  const [modalShow, setModalShow] = useState(false);
+  return (
+    <>
+      <button className='modalButton' onClick={() => setModalShow(true)}>
+        <div className={`card-div col-xs-12 col-sm-6 col-lg-4 ${props.class}`}>
+          {
+            /* </div>/<a href={props.href} target='_blank' rel='noreferrer'> */
+            <div className='card'>
+              <img src={process.env.PUBLIC_URL + props.img} alt={props.alt} />
+              <div className='card-body'>
+                <h5 className='card-title'>{props.title}</h5>
+              </div>
+            </div>
+            /* </a> */
+          }
+        </div>
+      </button>
+      <MyModal
+        show={modalShow}
+        title={props.title}
+        onHide={() => setModalShow(false)}
+      />
+    </>
+  );
+};
 
 export default Card;
--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,33 +1,37 @@
-import React from "react";
-import Card from "../Card";
-import projects from "../../utils/projects.js";
-import "./style.css";
-import Pdf from "../../documents/EthanResume.PDF";
-
-
+import React, { useState } from 'react';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+import Card from '../Card';
+import projects from '../../utils/projects.js';
+import './style.css';
+import Pdf from '../../documents/EthanResume.PDF';
 
 function createCard(project) {
-    return(
-        <Card
-            key={project.id}
-            id={project.id}
-            title={project.title}
-            href={project.href}
-            img={project.img}
-            alt={project.alt}
-            class={project.class}
-        />
-    );
+  return (
+    <Card
+      key={project.id}
+      id={project.id}
+      title={project.title}
+      href={project.href}
+      img={project.img}
+      alt={project.alt}
+      class={project.class}
+    />
+  );
 }
 
 function Portfolio() {
-    return(
-        <div className="container" id="portfolio-box">
-            <h3 className="portfolio-title">Featured Projects:</h3>
-            <div className="row">{projects.map(createCard)}</div>
-            <a className="pdf-link" href= {Pdf} target="_blank" rel="noreferrer"><h3 className="resume-link">My Resume</h3></a>
-        </div>
-    )
+  return (
+    <div className='container' id='portfolio-box'>
+      <h3 className='portfolio-title'>Featured Projects:</h3>
+
+      <div className='row'>{projects.map(createCard)}</div>
+
+      <a className='pdf-link' href={Pdf} target='_blank' rel='noreferrer'>
+        <h3 className='resume-link'>My Resume</h3>
+      </a>
+    </div>
+  );
 }
 
 export default Portfolio;
